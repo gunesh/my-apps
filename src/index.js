51ccd8c3b4598accec61ab127353fd900b69ef28
gunesh <gunesh26@gmail.com>
2023-05-14 01:01:45 +0530

    2 files modified, 4 added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './utility/app-context';
 import './assets/css/style.scss';
 import App from './App';
+import { ExternalStore } from './redux/store';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <Provider store={ExternalStore}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
